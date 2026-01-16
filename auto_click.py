e718001e6b188a6b2081050875040c6cf3da9db2
lizihao <2502904791@qq.com>
2026-01-16 17:37:59 +0800

    Add huijuan

--- auto_click.py
+++ auto_click.py
@@ -66,7 +66,7 @@ def random_click(x, y, random_range = 40):
     pyautogui.click(click_x, click_y)
 
 
-def xiezuo(shared_gouyu_count, shared_jinbi_count, lock):
+def xiezuo(gouyu_count, lock):
     # 一直监听勾玉协作
     gouyu_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "gouyu.png")
     jieshou_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "jieshou.png")
@@ -83,7 +83,8 @@ def xiezuo(shared_gouyu_count, shared_jinbi_count, lock):
                     screenshot.save(f"screenshot/{now_time}.png")
                     jieshou_x, jieshou_y = jieshou_result
                     random_click(jieshou_x, jieshou_y)
-                    shared_gouyu_count.value += 1
+                    gouyu_count.value += 1
+                    # 再加上飞书或者钉钉通知操作
                     time.sleep(1)
             else:
                 jujue_result = find_image_on_screen(jujue_path)
@@ -94,8 +95,30 @@ def xiezuo(shared_gouyu_count, shared_jinbi_count, lock):
                     screenshot.save(f"screenshot/{now_time}.png")
                     jujue_x, jujue_y = jujue_result
                     random_click(jujue_x, jujue_y)
-                    shared_jinbi_count.value += 1
                     time.sleep(1)
+
+
+def huijuan(xiaoHuiJuan_count, zhongHuiJuan_count, daHuiJuan_count, lock):
+    # 一直绘卷
+    xiaoHuiJuan = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "xiaoHuiJuan.png")
+    zhongHuiJuan = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "zhongHuiJuan.png")
+    daHuiJuan = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "daHuiJuan.png")
+    while True:
+        xiaoHuiJuan_result = find_image_on_screen(xiaoHuiJuan)
+        zhongHuiJuan_result = find_image_on_screen(zhongHuiJuan)
+        daHuiJuan_result = find_image_on_screen(daHuiJuan)
+        if xiaoHuiJuan_result or zhongHuiJuan_result or daHuiJuan_result:
+            with lock:
+                if xiaoHuiJuan_result:
+                    xiaoHuiJuan_count.value += 1    
+                    time.sleep(5) 
+                elif zhongHuiJuan_result:
+                    zhongHuiJuan_count.value += 1    
+                    time.sleep(5)
+                elif daHuiJuan_result:
+                    daHuiJuan_count.value += 1    
+                    time.sleep(5)
+
 
 def main():
     end_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "end.png")
@@ -106,112 +129,136 @@ def main():
     xiezhan_path = ""
     choice1 = ""
     choice2 = ""
+    choice3 = ""
+    scan_huijuan = True
 
     choice1 = input("请输入1或2。1：打活动，2：打御魂或御灵副本: ").strip()
     if choice1 == "1":
-        choice2 = input("请输入y或n。y：可以点击协战，n：不可以点击协战: ").strip().lower()
-        if choice2 == "y":
-            start_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_start.png")
-            shishenlu_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_shishenlu.png")
-            xiezhan_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_xiezhan.png")
-            print("当前模式为：打活动副本且可以点击协战界面。请注意：每次打新活动副本前记得更新图标。")
-        else:
-            start_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_start.png")
-            shishenlu_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_shishenlu.png")
-            print("当前模式为：打活动副本，但是不点击协战界面。请注意：每次打新活动副本前记得更新图标。")
-    else:
+        start_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_start.png")
+        scan_huijuan = False
+    elif choice1 == "2":
         start_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "yu_start.png")
-        shishenlu_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "yu_shishenlu.png")
-        print("当前模式为：打御魂或者御灵。")
+    else:
+        pass
+
+    choice2 = input("请输入y或n。y：可以点击协战，n：不可以点击协战: ").strip().lower()
+    if choice2 == "y":
+        xiezhan_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_xiezhan.png")
+    else:
+        pass
+
+    choice3 = input("请输入y或n。y：可以点击式神录，n：不可以点击式神录: ").strip().lower()
+    if choice3 == "y":
+        shishenlu_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imgs", "hd_shishenlu.png")
+    else:
+        pass
         
     run_count = 0
 
     # 启动识别协作子进程，接受勾玉协作，拒绝金币协作
     gouyu_count = Value('i', 0)  # 'i' 表示整型
-    jinbi_count = Value('i', 0)
-    lock = Lock()
-    process = Process(target=xiezuo, args=(gouyu_count, jinbi_count, lock))
-    process.daemon = True
-    process.start()
+    lock1 = Lock()
+    process1 = Process(target=xiezuo, args=(gouyu_count, lock1))
+    process1.daemon = True
+    process1.start()
+
+    if scan_huijuan:
+        xiaoHuiJuan_count = Value('i', 0)  # 'i' 表示整型
+        zhongHuiJuan_count = Value('i', 0)
+        daHuiJuan_count = Value('i', 0)
+        lock2 = Lock()
+        process2 = Process(target=huijuan, args=(xiaoHuiJuan_count, zhongHuiJuan_count, daHuiJuan_count, lock2))
+        process2.daemon = True
+        process2.start()
     
     try:
         while True:
             if run_count > 10000:
-                print("已执行100次，退出", flush=True)
+                print("已执行10000次，退出", flush=True)
                 break
             # 识别start.png
             delay = 0
-            while True:
-                time.sleep(3)
-                start_result = find_image_on_screen(start_path)
-                if start_result:
-                    # 随机延迟1-3秒内，5%概率点击式神录，5%概率点击协战，10%概率等待5-10秒
-                    possible =  random.random()
-                    if possible < 0.05:
-                        print("触发5%概率, 点击式神录并等待10-15s后返回", flush=True)
-                        shishenlu_result = find_image_on_screen(shishenlu_path)
-                        if shishenlu_result:
-                            shishenlu_x, shishenlu_y = shishenlu_result
-                            random_click(shishenlu_x, shishenlu_y)
-                        # 等待10-15s并等待页面加载
-                        time.sleep(random.uniform(10, 15))
-                        # 点击返回
-                        fanhui_result = find_image_on_screen(fanhui_path)
-                        if fanhui_result:
-                            fanhui_x, fanhui_y = fanhui_result
-                            random_click(fanhui_x, fanhui_y)
-                        # 等待5s页面加载
-                        time.sleep(5)
-                    elif 0.05 <= possible < 0.1 and choice2 == "y":
-                        print("触发5%概率, 点击协战并等待10-15s后返回", flush=True)
-                        xiezhan_result = find_image_on_screen(xiezhan_path)
-                        if xiezhan_result:
-                            xiezhan_x, xiezhan_y = xiezhan_result
-                            random_click(xiezhan_x, xiezhan_y)
-                        # 等待10-15s并等待页面加载
-                        time.sleep(random.uniform(10, 15))
-                        # 点击返回
-                        guanbi_result = find_image_on_screen(guanbi_path, 0.7)
-                        if guanbi_result:
-                            guanbi_x, guanbi_y = guanbi_result
-                            random_click(guanbi_x, guanbi_y)
-                        # 等待5s页面加载
-                        time.sleep(5)
-                    elif 0.1 <= possible < 0.2:
-                        delay = random.uniform(5, 10)
-                        print(f"触发10%概率，等待{delay:.2f}秒", flush=True, end="-->")
-                    else:
-                        delay = random.uniform(1, 3)
-                        print(f"等待{delay:.2f}秒", flush=True, end="-->")
-                    time.sleep(delay)
-                    x, y = start_result
-                    random_click(x, y)
-                    break
+            start_result = find_image_on_screen(start_path)
+            if start_result:
+                # 随机延迟1-3秒内，5%概率点击式神录，5%概率点击协战，10%概率等待5-10秒
+                possible =  random.random()
+                if possible < 0.05 and choice3 == "y":
+                    print("触发5%概率, 点击式神录并等待10-15s后返回", flush=True)
+                    shishenlu_result = find_image_on_screen(shishenlu_path)
+                    if shishenlu_result:
+                        shishenlu_x, shishenlu_y = shishenlu_result
+                        random_click(shishenlu_x, shishenlu_y)
+                    # 等待10-15s并等待页面加载
+                    time.sleep(random.uniform(10, 15))
+                    # 点击返回
+                    fanhui_result = find_image_on_screen(fanhui_path)
+                    if fanhui_result:
+                        fanhui_x, fanhui_y = fanhui_result
+                        random_click(fanhui_x, fanhui_y)
+                    # 等待5s页面加载
+                    time.sleep(5)
+                elif 0.05 <= possible < 0.1 and choice2 == "y":
+                    print("触发5%概率, 点击协战并等待10-15s后返回", flush=True)
+                    xiezhan_result = find_image_on_screen(xiezhan_path)
+                    if xiezhan_result:
+                        xiezhan_x, xiezhan_y = xiezhan_result
+                        random_click(xiezhan_x, xiezhan_y)
+                    # 等待10-15s并等待页面加载
+                    time.sleep(random.uniform(10, 15))
+                    # 点击返回
+                    guanbi_result = find_image_on_screen(guanbi_path, 0.7)
+                    if guanbi_result:
+                        guanbi_x, guanbi_y = guanbi_result
+                        random_click(guanbi_x, guanbi_y)
+                    # 等待5s页面加载
+                    time.sleep(5)
+                elif 0.1 <= possible < 0.2:
+                    delay = random.uniform(5, 10)
+                    print(f"触发10%概率，等待{delay:.2f}秒", flush=True, end="-->")
                 else:
-                    pass
-            # 识别end.png
-            try_time = 1
-            while True:
-                # 由一场战斗时间决定，其基础上加6-10s
-                delay = random.uniform(14, 18)
-                print(f"第{try_time}次等待{delay:.2f}s", flush=True, end="-->")
+                    delay = random.uniform(1, 3)
+                    print(f"等待{delay:.2f}秒", flush=True, end="-->")
                 time.sleep(delay)
-                end_result = find_image_on_screen(end_path)
-                if end_result:
-                    end_x, end_y = end_result
-                    random_click(end_x, end_y)
-                    # time.sleep(3) # 特殊活动，需要间隔点击两次
-                    # random_click(end_x, end_y)
-                    run_count += 1
-                    with lock:
-                        current_gouyu_count = gouyu_count.value
-                        current_jinbi_count = jinbi_count.value
-                    print(f"运行{run_count}次，勾玉{current_gouyu_count}次, 金币{current_jinbi_count}次")
-                    break
-                else:
-                    try_time += 1
-                    if try_time > 3:
-                        break
+                x, y = start_result
+                random_click(x, y)
+
+                # 识别end.png
+                try_time = 1
+                while True:
+                    # 由一场战斗时间决定，其基础上加12-16s
+                    delay = random.uniform(14, 18)
+                    print(f"第{try_time}次等待{delay:.2f}s", flush=True, end="-->")
+                    time.sleep(delay)
+                    end_result = find_image_on_screen(end_path)
+                    if end_result:
+                        end_x, end_y = end_result
+                        random_click(end_x, end_y)
+                        time.sleep(0.5) # 防止时间恰好，点击两次
+                        random_click(end_x, end_y)
+                        run_count += 1
+                        with lock1:
+                            current_gouyu_count = gouyu_count.value
+                        if scan_huijuan:
+                            with lock2:
+                                current_xiaohuijuan_count = xiaoHuiJuan_count.value
+                                current_zhonghuijuan_count = zhongHuiJuan_count.value
+                                current_dahuijuan_count = daHuiJuan_count.value
+                        if current_gouyu_count > 0:
+                            if scan_huijuan:
+                                print(f"运行{run_count}次，接到勾协，小绘卷 {current_xiaohuijuan_count} 次, 中绘卷 {current_zhonghuijuan_count} 次, 大绘卷 {current_dahuijuan_count} 次")
+                            else:
+                                print(f"运行{run_count}次，接到勾协")
+                        else:
+                            if scan_huijuan:
+                                print(f"运行{run_count}次，小绘卷 {current_xiaohuijuan_count} 次, 中绘卷 {current_zhonghuijuan_count} 次, 大绘卷 {current_dahuijuan_count} 次")
+                            else:
+                                print(f"运行{run_count}次")
+                    else:
+                        try_time += 1
+                        if try_time > 3:
+                            break
+            else:
+                pass
     except KeyboardInterrupt:
         print("脚本已停止")
 
